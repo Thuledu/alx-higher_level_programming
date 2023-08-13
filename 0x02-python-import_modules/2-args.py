89c42f3df9219189b986681b0a45b6870cfed7db
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-08-12 20:29:40 -0700

    Python - import & modules tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 import sys
-print(f"Number of arguments: {len(sys.argv)}")
-if len(sys.argv) == 0:
-    print(".")
-else:
-    for i in range(1, len(sys.argv)):
-        print(f"Argument {i}: {sys.argv[i]}")
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arguments', nargs='*', help='List of arguments')
+    args = parser.parse_args()
+
+    size = len(args.arguments)
+
+    if size > 1:
+        print(f"{size} arguments:")
+        for i, arg in enumerate(args.arguments, 1):
+            print(f"{i}: {arg}")
+
+    elif size == 0:
+        print(f"{size} arguments.")
+
+    else:
+        print(f"{size} argument:")
+        print(f"{size}: {args.arguments[0]}")
+
+if __name__ == "__main__":
+    main()
