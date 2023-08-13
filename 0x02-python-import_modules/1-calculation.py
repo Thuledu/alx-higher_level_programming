d1f412e1088b2f244e6d4057583fb4f6026570c1
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-08-12 19:13:14 -0700

    Python - import & modules tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
 add_result = add(a, b)
-subtract_result = subtract(a, b)
-multiply_result = multiply(a, b)
-divide_result = divide(a, b)
+subtract_result = sub(a, b)
+multiply_result = mul(a, b)
+divide_result = div(a, b)
 
 print(f"{a} + {b} = {add_result}")
 print(f"{a} - {b} = {subtract_result}")
