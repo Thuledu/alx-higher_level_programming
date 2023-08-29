3a5cbff2cc023bedf0feabf4e505f45bfbe07ff8
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-08-29 22:20:32 +0000

    Python - Classes and Objects Tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """An empty class Square that defines a square"""
 class Square:
-    def __init__(self, size):
- """Constructor.
-	Args:
-		size: the length of side of the square."""
-        self.__size = size
+	def __init__(self, size):
+		self.__size = size
