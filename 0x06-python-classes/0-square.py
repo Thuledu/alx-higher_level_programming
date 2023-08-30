0698afa57ad97f1e250f0801214bdb0911c0b1fe
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-08-30 07:22:58 +0000

    Python - Classes and Objects Tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""An empty class Square that defines a square"""
+"""
+An empty class Square that defines a square.
+
+"""
+
+
 class Square:
     """
-    A class that defines a square.
-    
-    Attributes:
-        None
-    """
-    pass
+    An empty class Square that defines a square.
 
+    """
+
+    pass
