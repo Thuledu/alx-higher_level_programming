e454b31ac2771f6b6445eb4c9acf185ae68764e8
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-09-12 17:45:40 +0000

    Python - Input/Output Tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-	with open(filename, "r", encoding="utf8") as file:
-		for line in file:
-			print(line, end="")
+  with open(filename, "r", encoding="utf-8") as f:
+    text = f.read()
+    print(text)
