fcbafe311c62d5d4b014ee333206e040c5bf540e
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-09-14 02:20:04 +0000

    Python - Inheritance Task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+
 def lookup(obj):
 	"""
-    Returns a list of available attributes and methods of an object.
-
-    Parameters:
-    obj (object): The object to inspect.
-
-    Returns:
-    list: A list of strings containing the names of the attributes and methods of the object.
+    Returns: A list of strings containing the names of the attributes and methods of the object.
 	"""
-	return dir(obj)
+	return (dir(obj))
 
