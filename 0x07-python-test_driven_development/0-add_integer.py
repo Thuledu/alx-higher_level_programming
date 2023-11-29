3bf359045361dae8fed893db46cfdd0b430da49a
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-11-28 19:06:59 -0800

    Test-driven Development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
+
+def add_integer(a: int, b: int = 98) -> int:
     """
     Adds two integers.
 
-    Parameters:
-    a (int or float): The first integer to be added.
-    b (int or float): The second integer to be added. Default value is 98.
-
-    Returns:
-    int: The addition of a and b.
+    Args:
+        a (int or float): First integer or float.
+        b (int or float, optional): Second integer or float. Defaults to 98.
 
     Raises:
-    TypeError: If a or b is not an integer or float.
+        TypeError: If a or b is not an integer or float.
+
+    Returns:
+        int: The addition of a and b.
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
-    # Check if a and b are integers or floats
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    a = int(a)
+    b = int(b)
 
-    # Cast a and b to integers if they are floats
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    # Add the integers
-    result = a + b
-
-    return result
+    return a + b
 
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
-def matrix_divided(matrix, div):
+
+from typing import List, Union
+
+def matrix_divided(matrix: List[List[Union[int, float]]], div: Union[int, float]) -> List[List[Union[int, float]]]:
     """
-    Divides all elements of a matrix by a number.
+    Divides all elements of a matrix by a given divisor.
 
-    Parameters:
-    matrix (list of lists): The matrix to be divided.
-    div (int or float): The number to divide the matrix elements by.
-
-    Returns:
-    list of lists: The new matrix with elements divided by div.
+    Args:
+        matrix (List[List[Union[int, float]]]): The matrix to be divided.
+        div (Union[int, float]): The divisor.
 
     Raises:
-    TypeError: If matrix is not a list of lists of integers or floats,
-               or if div is not a number.
-    ZeroDivisionError: If div is equal to 0.
-    """
+        TypeError: If matrix is not a list of lists of integers or floats,
+                   or if each row of the matrix does not have the same size,
+                   or if div is not a number.
+        ZeroDivisionError: If div is equal to zero.
 
-    # Check if matrix is a list of lists of integers or floats
-    if not all(isinstance(row, list) for row in matrix) or \
-            not all(isinstance(num, (int, float)) for row in matrix for num in row):
+    Returns:
+        List[List[Union[int, float]]]: The new matrix with all elements divided by div.
+    """
+    # Validate the matrix
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if each row of the matrix has the same size
-    row_sizes = set(len(row) for row in matrix)
-    if len(row_sizes) > 1:
+    # Check if each row has the same size
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Check if div is a number
+    # Validate the divisor
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    # Check if div is not equal to 0
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Divide each element of the matrix by div, rounded to 2 decimal places
-    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+    # Divide each element of the matrix by div and round to 2 decimal places
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
 
     return new_matrix
 
