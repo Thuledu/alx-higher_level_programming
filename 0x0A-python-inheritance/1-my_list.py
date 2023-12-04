28c393d0ee83afeea8a84fbe305830d3e2037efe
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-04 08:17:54 -0800

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""A class MyList that inherits from list."""
 
 class MyList(list):
     """A class that inherits from list and adds a method to print the list in sorted order."""
