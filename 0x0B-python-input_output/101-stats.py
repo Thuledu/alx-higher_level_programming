8d72e102cc8c37ca80b63cf180765e0a5dff46bb
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-05 09:08:50 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """A cript that reads stdin line by line and computes metrics."""
 
-import sys
-
 def print_statistics(total_size, status_codes):
     """
     Prints the statistics based on the total file size and the number of lines for each status code.
@@ -16,23 +14,29 @@ def print_statistics(total_size, status_codes):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
 
-total_size = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
-try:
-    for i, line in enumerate(sys.stdin, 1):
-        try:
-            parts = line.split()
-            total_size += int(parts[-1])
-            status_code = parts[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except Exception:
-            pass
+if __name__ == "__main__":
+    import sys
 
-        if i % 10 == 0:
-            print_statistics(total_size, status_codes)
+    count = 0
+    total_size = 0
+    status_codes = {}
+    valid_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
-except KeyboardInterrupt:
-    print_statistics(total_size, status_codes)
-    raise
+    try:
+        for i, line in enumerate(sys.stdin, 1):
+            try:
+                parts = line.split()
+                total_size += int(parts[-1])
+                status_code = parts[-2]
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+            except Exception:
+                pass
+
+            if i % 10 == 0:
+                print_statistics(total_size, status_codes)
+
+            except KeyboardInterrupt:
+                print_statistics(total_size, status_codes)
+                raise
