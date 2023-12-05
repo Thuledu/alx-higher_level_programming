fce2a81e69b713ca117e13240a79f884f85b71a2
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-05 08:17:51 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""A function that creates an Object from a “JSON file”."""
+
 import json
 
 def load_from_json_file(filename):
-	with open(filename, "r") as file:
-		return json.load(file)
+    """
+    Create an object from a JSON file.
+
+    Args:
+    - filename: The name of the JSON file to load the object from.
+
+    Returns:
+    - The Python data structure represented by the JSON in the file.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
