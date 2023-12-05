aaf5f5780079baa74b90f1c04012130fd7d3b5bf
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-05 08:12:22 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""A function that returns an object (Python data structure) represented by a JSON string."""
+
 import json
 
 def from_json_string(my_str):
-	return json.loads(my_str)
+    """
+    Return a Python data structure represented by a JSON string.
+
+    Args:
+    - my_str: The JSON string to be deserialized.
+
+    Returns:
+    - A Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
