68d488f82daecc95d389588498f8dd9db62c88d9
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-04 18:32:19 -0800

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,52 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle.
+    """Represents a Rectangle class."""
+    def __init__(self, width, height):
+        """Initializes a Rectangle instance."""
+        super().__init__()
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
 
-    This class inherits from the BaseGeometry class and provides functionality to calculate
-    the area of a rectangle.
+    def area(self):
+        """Calculates the area of the Rectangle."""
+        return self.__width * self.__height
 
-    Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-    Raises:
-        TypeError: If the width or height is not an integer.
-        ValueError: If the width or height is less than or equal to 0.
-    """
-
-	def __init__(self, width, height):
-		self.__width = self.integer_validator("width", width)
-		self.__width = width
-		self.__height = self.integer_validator("height", height)
-		self.__height = height
-
-	def area(self):
-        """
-        Calculate the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-		return self.__width * self.__height
-
-	def __str__(self):
-        """
-        Return the string representation of the rectangle.
-
-        Returns:
-            str: The rectangle description.
-        """
-		return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-	def __repr__(self):
-        """
-        Return the string representation of the rectangle.
-
-        Returns:
-            str: The rectangle description.
-        """
-		string += str(self.__width) + "/" + str(self.__height)
-		return string
+    def __str__(self):
+        """Returns the rectangle description."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
