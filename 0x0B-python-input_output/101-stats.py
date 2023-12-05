5d9fc28be2c93a32bdb14acf5af76a1e0ec1b2d5
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-05 09:13:27 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -16,10 +16,6 @@ def print_statistics(total_size, status_codes):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
 
-
-if __name__ == "__main__":
-    import sys
-
     total_size = 0
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
