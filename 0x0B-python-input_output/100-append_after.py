39bbc83ba7d347eda59a4455cbb1ef4df81f8e8e
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-05 08:47:26 -0800

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""A function that inserts a line of text to a file, after each line containing a specific string."""
 def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file after each line containing a specific string.
+
+    Args:
+    - filename: The name of the file to modify.
+    - search_string: The specific string to search for in the file.
+    - new_string: The line of text to insert after each line containing the search string.
+    """
     with open(filename, 'r') as file:
         lines = file.readlines()
-    
+
     with open(filename, 'w') as file:
         for line in lines:
             file.write(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                file.write(new_string)
