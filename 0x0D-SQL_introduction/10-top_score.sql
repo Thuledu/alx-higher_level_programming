1b19126f4f4c6dddc8c4070e5aa18b3e8bc9e792
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-01-16 12:40:58 -0800

    SQL Introduction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT name, score
-FROM hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT AVG(score) AS average
-FROM hbtn_0c_0.second_table;
+FROM second_table;
 
