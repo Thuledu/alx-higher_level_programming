dbbab0254d9768c21b0431bdcc3cb9a3187748ef
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-02-12 18:40:27 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 /* A script that searches the second biggest integer in the list of arguments. */
-const args = process.argv.slice(2).map(Number);
-
-if (args.length <= 1) 
+if (process.argv.length <= 3) 
 {
-	console.log(0);
+	console.log('0');
 } 
 else 
 {
-	const sortedArgs = args.sort((a, b) => b - a);
-	console.log(sortedArgs[1]);
+	const arr = process.argv.slice(2).map(Number);
+	const second = arr.sort(function (a, b) { return b - a; })[1];
+	console.log(second);
 }
