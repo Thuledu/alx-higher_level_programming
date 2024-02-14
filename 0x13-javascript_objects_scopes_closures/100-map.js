e99a58b3ad37a62a8cc726d773e48526a8f6c090
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-02-14 02:53:19 +0200

    0x13. JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* A script that imports an array and computes a new array. */
+const list = require('./100-data').list;
+
+console.log('Initial list:', list);
+
+const newList = list.map((value, index) => value * index);
+
+console.log('New list:', newList);
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/* A class Rectangle that defines a rectangle. */
+module.exports = class Rectangle {
+	constructor(w, h) 
+	{
+		if (w > 0 && h > 0)
+		{
+			this.width = w;
+			this.height = h;
+		} 
+		else 
+		{
+			Object.create(null);
+		}
+	}
+
+	print()
+	{
+		if (this.width === 0 || this.height === 0) 
+		{
+			console.log('');
+		} 
+		else 
+		{
+			for (let i = 0; i < this.height; i++) 
+			{
+				let row = '';
+				for (let j = 0; j < this.width; j++) 
+				{
+					row += 'X';
+				}
+				console.log(row);
+			}
+		}
+	}
+}
